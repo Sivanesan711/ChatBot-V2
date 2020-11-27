480c74b5c3b6d49dbbbd0e7365b2b43425326131
SIVANESAN-DHANIKACHALAM <sivanesan_dh@srmuniv.edu.in>
2020-11-27 20:45:06 +0530

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChatBotComponent } from './Component/chat-bot/chat-bot.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatBotComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
